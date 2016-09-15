948fe2437ea75ef9ccc0fee29e89e2c4e60b8ff7
Zebbeni <andrew.albers@gmail.com>
2016-09-14 22:11:15 -0600

    simplify, add RobotTasks class

---
+++ src/team006/RobotTasks.java
@@ -0,0 +1,38 @@
+package team006;
+
+import battlecode.common.*;
+
+/**
+ * Created by andrewalbers on 9/14/16.
+ */
+public class RobotTasks {
+
+    public static int TASK_NOT_GIVEN = -1;
+    public static int TASK_IN_PROGRESS = 0;
+    public static int TASK_COMPLETE = 1;
+    public static int TASK_ABANDONED = 2;
+
+    // Move toward a location
+    public static int moveToLocation(RobotController rc, MapLocation targetLocation) {
+        try {
+            Direction dirToMove = rc.getLocation().directionTo(targetLocation);
+            if (rc.canMove(dirToMove)) {
+                rc.move(dirToMove);
+            } else if (rc.senseRubble(rc.getLocation().add(dirToMove)) >= GameConstants.RUBBLE_OBSTRUCTION_THRESH) {
+                // If there's rubble in this direction, clear it
+                rc.clearRubble(dirToMove);
+            } else if (rc.getLocation().equals(targetLocation)) {
+                // If goal reached
+                return TASK_COMPLETE;
+            } else {
+                // If can't move there and no rubble to clear...
+                return TASK_ABANDONED;
+                // TODO: don't assume this is a map border. Could be something else blocking path
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return TASK_IN_PROGRESS;
+    }
+}
